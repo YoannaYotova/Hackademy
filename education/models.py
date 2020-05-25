de8d4b279b083f6451f805c11b3b4f0759f8c6cc
Yoanna <yoansiyotova@gmail.com>
2020-05-25 20:56:13 +0300

    Add Course, Lecture and Task models

--- education/models.py
+++ education/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    description = models.TextField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'Course "{self.name}"'
+
+    @property
+    def duration(self):
+        if self.end_date:
+            return self.end_date - self.start_date
+
+
+class Lecture(models.Model):
+    name = models.CharField(max_length=255)
+    week = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    due_date = models.DateTimeField(null=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
