4a3e55904cf6d213c647210c9813163b930ad72b
Yoanna <yoansiyotova@gmail.com>
2020-05-26 12:15:13 +0300

    Add Solution Admin

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from education.models import Course, Lecture, Task, Solution
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_duration', 'start_date', 'end_date')
+
+    def get_duration(self, obj):
+        if obj.duration:
+            return f'{obj.duration.days // 30} months'
+
+        return 'N/A'
+
+    get_duration.short_description = 'Duration'
+
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'week', 'course', 'url')
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'due_date', 'course', 'lecture')
+
+
+@admin.register(Solution)
+class SolutionAdmin(admin.ModelAdmin):
+    list_display = ('task', 'get_description', 'get_lecture', 'date', 'url')
+
+    def get_description(self, obj):
+        return obj.task.description
+
+    def get_lecture(self, obj):
+        return obj.task.lecture
+
+    get_description.short_description = 'description'
+    get_lecture.short_description = 'lecture'
