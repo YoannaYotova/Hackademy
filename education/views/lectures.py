c16a6f57880ceda099a0605d69a2d1bbaaf4c5ef
Yoanna <yoansiyotova@gmail.com>
2020-05-27 21:32:33 +0300

    Add lectures view

---
+++ education/views/lectures.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+# from django.views.generic import CreateView
+
+from education.models import Lecture
+
+
+def list(request):
+    return render(request, 'lectures/list.html', {'lectures': Lecture.objects.all()})
+
+
+def detail(request, lecture_id):
+    lecture = get_object_or_404(Lecture, id=lecture_id)
+    return render(request, 'lectures/detail.html', {'lecture': lecture})
